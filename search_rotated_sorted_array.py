fa530bfc979e27d5441f953eab65da2072597624
gamagegit <cgamage92@yahoo.com>
2022-12-06 22:15:07 -0800

    partially sorted binary search solution

---
+++ search_rotated_sorted_array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def search(self, nums, target) -> bool:
+        
+        if(target > nums[-1] or target < nums[0]):
+            return False
+        
+        left = 0
+        right = len(nums)-1
+        
+        if target == nums[left] or target == nums[right]:
+            return True
+        # this will find the pivot
+        i = 0
+        while nums[i]< nums[i+1]:
+            i +=1
+        i+=1
+            
+        if target > left:
+            right = i-1
+        else:
+            left = i
+        
+        def search(target,num,left,right):
+            if right-left < 5:
+                for i in range(left,right+1):
+                    if i == target:
+                        return True
+                return False
+            else:
+                midPoint = (left + right)//2
+                if target < nums[midPoint]:
+                    search(target,num,left,midPoint)
+                else:
+                    search(target,num,midPoint,right)
+        return search(target,nums,left,right)
+    
+obj = Solution()
+
+nums = [2,5,6,0,0,1,2]
+target = 2
+
+print(obj.search(nums,target))
